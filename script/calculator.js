216eb78628017a45980aee9efc67e4f6ab912562
pietrzakacper <kacpietrzak@gmail.com>
2016-08-18 09:18:01 +0200

    Added some styling and improved daling with floating point numbers

--- script/calculator.js
+++ script/calculator.js
@@ -6,6 +6,11 @@ var calculator=(function(){
     var arg1=0;
     var arg2 =0;
 
+    var arg1Precision=1;
+    var arg2Precision=1;
+
+    var isCommaPresent = false;
+
     var operatorClicked = false;
     //cache DOM
     var $screen=$('.screen');
@@ -14,9 +19,12 @@ var calculator=(function(){
     //bind events
     $keyboard.on('click',".digit",addToCaluculation);
     $keyboard.on('click',".binary",setBinaryOperator);
-  //  $keyboard.on('click',".comma",makeFloat);
+    $keyboard.on('click',".comma",makeFloat);
   //  $keyboard.on('click',".func",reset);
     $keyboard.on('click',".equals",calculate);
+
+    _render();
+
     function _render(){
       $screen.html(input.join(""));
     }
@@ -27,18 +35,27 @@ var calculator=(function(){
       } else {
         input.push($(event.target).html());
       }
+      if(isCommaPresent){
+        if(operatorClicked){
+          arg2Precision++;
+        } else{
+          arg1Precision++;
+        }
+      }
       _render();
     }
 
     function setBinaryOperator(event){
       if(operatorClicked)return;
       operatorClicked = true;
+      isCommaPresent = false;
 
-      arg1=parseInt(input.join(""));
-      console.log(arg1);
+      arg1=parseFloat(input.join(""));
       input = [0];
       _render();
       var $operator = $(event.target);
+
+      $keyboard.find(".binary").removeClass("key-hover");
       $operator.parent().addClass("clicked");
       switch($operator.html()){
         case 'x':
@@ -57,10 +74,22 @@ var calculator=(function(){
     }
 
     function calculate(){
-      arg2=parseInt(input.join(""));
-      console.log(arg2);
-      console.log(operation(arg1,arg2));
-      input = (""+operation(arg1,arg2)).split("");
+      arg2=parseFloat(input.join(""));
+      // console.log(arg1+" "+arg1Precision+" "+arg2+" "+arg2Precision);
+      // console.log(operation(arg1,arg2));
+      var result = operation(arg1,arg2).toPrecision(2*((arg1Precision>arg2Precision)?arg1Precision:arg2Precision));
+      // console.log(result);
+      result = parseFloat(""+result);
+      input = (""+result).split("");
+
+      $keyboard.find(".binary").addClass("key-hover");
+      $keyboard.find(".clicked").removeClass("clicked");
+      _render();
+    }
+
+    function makeFloat(){
+      input.push('.');
+      isCommaPresent = true;
       _render();
     }
 
